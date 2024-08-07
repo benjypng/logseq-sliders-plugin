048db6e0ce271d9b3d55674979a8c1546400a5cc
Benjy <ben@pngs.cc>
2024-08-07 19:30:18 +0800

    perf: used traditional renderer
    BREAKING CHANGE: used traditional renderer

--- src/settings.ts
+++ src/settings.ts
@@ -8,11 +8,4 @@ export const settings: SettingSchemaDesc[] = [
     title: 'Default Property Key',
     description: 'Indicate the property key associated with the slider value.',
   },
-  {
-    key: 'hidePropertyKey',
-    type: 'boolean',
-    default: false,
-    title: 'Hide Property Key',
-    description: 'Hide the slider value from the block properties.',
-  },
 ]
